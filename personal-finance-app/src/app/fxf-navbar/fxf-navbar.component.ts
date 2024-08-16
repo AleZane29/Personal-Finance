eb56910fde6687c676c1160db4ae6a5b7107962a
AleZane29 <alessio.zanella29@gmail.com>
2024-08-16 14:37:20 +0200

    Aggiunto DaisyUI, nuova sidebar e gestore tema

--- personal-finance-app/src/app/fxf-navbar/fxf-navbar.component.ts
+++ personal-finance-app/src/app/fxf-navbar/fxf-navbar.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'fxf-navbar',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, MatSlideToggleModule],
   templateUrl: './fxf-navbar.component.html',
   styleUrl: './fxf-navbar.component.scss',
 })
 export class FxfNavbarComponent {
   menuVisible: boolean = false;
+  theme = 'dark';
   OpenMenu() {
     this.menuVisible = this.menuVisible ? false : true;
+  }
+
+  toggleTheme() {
+    console.log(this.theme);
+    this.theme = this.theme == 'dark' ? 'light' : 'dark';
+    document
+      .getElementById('data-theme')
+      ?.setAttribute('data-theme', this.theme);
   }
 }
