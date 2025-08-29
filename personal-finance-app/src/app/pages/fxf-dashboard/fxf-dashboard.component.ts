5af0fd7ec5a05dfc6742531c4a6c7ba458e80bd0
AleZane29 <alessio.zanella29@gmail.com>
2025-08-29 14:50:10 +0200

    Corretto tema grafico

--- personal-finance-app/src/app/pages/fxf-dashboard/fxf-dashboard.component.ts
+++ personal-finance-app/src/app/pages/fxf-dashboard/fxf-dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { Transaction } from '../../interfaces/transactions';
+import { LocalStore } from '../../services/localStore.service';
+import { TransactionsService } from '../../services/transactions.service';
 
 @Component({
   selector: 'app-fxf-dashboard',
@@ -9,41 +12,115 @@ import Chart from 'chart.js/auto';
   styleUrl: './fxf-dashboard.component.scss',
 })
 export class FxfDashboardComponent implements OnInit {
-  public data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  constructor(
+    private service: TransactionsService,
+    private localStore: LocalStore
+  ) {}
 
-  public settings: any = {
-    type: 'bar',
-    options: {
-      animation: false,
-      plugins: {
-        legend: {
-          display: false,
-        },
-        tooltip: {
-          enabled: false,
-        },
-      },
-    },
-    data: {
-      labels: this.data.map((row) => row.year),
-      datasets: [
-        {
-          label: 'Acquisitions by year',
-          data: this.data.map((row) => row.count),
-        },
-      ],
-    },
-  };
+  dataIncome: Transaction[] = [];
+  theme: string = '';
+
+  public settings: any = {};
   chart: any;
   ngOnInit(): void {
-    this.chart = new Chart('acquisitions', this.settings);
+    this.service.GetAllIncome().subscribe({
+      next: (data) => {
+        this.dataIncome = data;
+
+        //#region Grafico
+        // Raggruppa e somma per categoria
+        const grouped = this.dataIncome.reduce((acc, row) => {
+          if (!acc[row.category]) {
+            acc[row.category] = 0;
+          }
+          acc[row.category] += row.amount;
+          return acc;
+        }, {} as Record<string, number>);
+
+        const labels = Object.keys(grouped);
+        const values = Object.values(grouped);
+
+        this.settings = {
+          type: 'bar',
+          options: {
+            animation: true,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                enabled: true,
+              },
+              datalabels: {
+                anchor: 'end',
+                align: 'end',
+                color: 'black',
+                font: {
+                  weight: 'bold',
+                },
+              },
+            },
+            scales: {
+              x: {
+                ticks: {
+                  font: {
+                    weight: 'bold',
+                    size: 14,
+                  },
+                },
+              },
+              y: {
+                ticks: {
+                  font: {
+                    weight: 'bold',
+                    size: 14,
+                  },
+                },
+              },
+            },
+          },
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: 'Income by category',
+                data: values,
+                borderWidth: 2,
+                borderRadius: 5,
+                borderSkipped: false,
+              },
+            ],
+          },
+        };
+        this.chart = new Chart('acquisitions', this.settings);
+      },
+      error: (err) => console.error('Errore:', err),
+    });
+
+    this.localStore.theme$.subscribe((theme) => {
+      if (!this.chart) return;
+      const isDark = theme === 'dark';
+
+      // this.chart.options.plugins!.legend!.labels!.color = isDark ? 'white' : undefined;
+      // this.chart.options.plugins!.tooltip!.titleColor = isDark ? 'white' : undefined;
+      // this.chart.options.plugins!.tooltip!.bodyColor = isDark ? 'white' : undefined;
+      // this.chart.options.plugins!.tooltip!.backgroundColor = isDark ? '#333' : undefined;
+
+      this.chart.options.scales!['x']!.ticks!.color = isDark
+        ? '#cfcfcf'
+        : undefined;
+      this.chart.options.scales!['y']!.ticks!.color = isDark
+        ? '#cfcfcf'
+        : undefined;
+      this.chart.options.scales!['x']!.grid!.color = isDark
+        ? '#7c7d83'
+        : '#cfcfcf';
+      this.chart.options.scales!['y']!.grid!.color = isDark
+        ? '#7c7d83'
+        : '#cfcfcf';
+
+      this.chart.update();
+    });
+    //#endregion
   }
 }
--- personal-finance-app/src/app/services/localStore.service.ts
+++ personal-finance-app/src/app/services/localStore.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Category } from '../interfaces/categories';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStore {
+  private themeSubject = new BehaviorSubject<string>(
+    this.getData('Theme') || 'light'
+  );
+  public theme$ = this.themeSubject.asObservable();
+
   constructor() {}
+
+  private isBrowser(): boolean {
+    return typeof window !== 'undefined' && !!window.localStorage;
+  }
 
   public saveData(key: string, value: string) {
     localStorage.setItem(key, value);
+    if (key === 'Theme') {
+      this.themeSubject.next(value);
+    }
   }
+
   //#region Categories
   public saveCategories(value: Category[]) {
     localStorage.setItem('Categories', JSON.stringify(value));
@@ -26,10 +40,12 @@ export class LocalStore {
     return JSON.parse(data);
   }
   //#endregion
+
   public getData(key: string) {
-    let data = localStorage.getItem(key) || '';
-    return data;
+    if (!this.isBrowser()) return '';
+    return localStorage.getItem(key) || '';
   }
+
   public removeData(key: string) {
     localStorage.removeItem(key);
   }
